9485c63ff0350132238def1a2a43b2d204d8e621
urion-st <oriol.torres.st23@gmail.com>
2026-01-14 18:32:54 +0100

    1.0

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const http = require('http');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+const TOKEN = process.env.TOKEN;
+const SEGUNDOS_SLOWMODE = 3600;
+
+client.on('threadCreate', async (thread) => {
+    try {
+
+        await thread.setRateLimitPerUser(SEGUNDOS_SLOWMODE);
+        console.log(`✅ Slow mode of ${SEGUNDOS_SLOWMODE} applied to ${thread.name}`);
+    } catch (error) {
+        console.error('❌ Error', error);
+    }
+});
+
+client.once('ready', () => {
+    console.log(`Logued as ${client.user.tag}`);
+});
+
+// Servidor web básico para que el hosting no lo apague
+http.createServer((req, res) => {
+    res.write("The bot is ready for war");
+    res.end();
+}).listen(8080);
+
+client.login(TOKEN);
