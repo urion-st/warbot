cc4433b053f42a835eba84c971d209932914c117
urion-st <oriol.torres.st23@gmail.com>
2026-01-14 18:49:16 +0100

    1.0.1

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ client.on('threadCreate', async (thread) => {
 
 client.once('ready', () => {
     console.log(`Logued as ${client.user.tag}`);
+    client.guilds.cache.forEach(async (guild) => {
+        const threads = await guild.channels.fetchActiveThreads();
+        threads.threads.forEach(async (thread) => {
+            if (thread.rateLimitPerUser !== SEGUNDOS_SLOWMODE) {
+                await thread.setRateLimitPerUser(SEGUNDOS_SLOWMODE);
+                console.log(`Actualizado hilo antiguo: ${thread.name}`);
+            }
+        });
+    });
 });
 
 // Servidor web básico para que el hosting no lo apague
@@ -27,5 +36,6 @@ http.createServer((req, res) => {
     res.write("The bot is ready for war");
     res.end();
 }).listen(8080);
+
 
 client.login(TOKEN);
